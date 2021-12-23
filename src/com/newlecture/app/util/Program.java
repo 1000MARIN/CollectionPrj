06455043fc81e144329f300c448793396b096f04
1000marin <leexjin@gmail.com>
2021-12-23 11:27:49 +0900

    Update Program.java

--- src/com/newlecture/app/util/Program.java
+++ src/com/newlecture/app/util/Program.java
@@ -3,7 +3,7 @@ package com.newlecture.app.util;
 public class Program {
 
 	public static void main(String[] args) {
-		 IntList list = new IntList();
+		ObjectList list = new ObjectList();
 		 list.add(3);
 		 list.add(5);
 		 int size = list.size();
@@ -14,9 +14,9 @@ public class Program {
 		 System.out.printf("size : %d\n", size); // clear한 후 상태
 		 
 		 list.add(7);
-		 int num = list.get(0);
+		 int num = (Integer) list.get(0);		// Object -> Integer
 		 System.out.printf("num : %d\n", num);	// 0번째 num값 출력
-		 num = list.get(1);						// 1번째 값 읽기 -> 1번째 값 없음  -> IndexOutOfBoundsException 오류 발생
+		 //num = (Integer) list.get(1);			// 1번째 값 읽기 -> 1번째 값 없음  -> IndexOutOfBoundsException 오류 발생
 	} // end of main
 } // end of Program
 
