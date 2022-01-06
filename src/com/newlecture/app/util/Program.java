0ec9b04830d4382fac357a92a2237551b9866c6d
1000marin <leexjin@gmail.com>
2022-01-06 14:56:59 +0900

    Update Program.java

--- src/com/newlecture/app/util/Program.java
+++ src/com/newlecture/app/util/Program.java
@@ -3,20 +3,24 @@ package com.newlecture.app.util;
 public class Program {
 
 	public static void main(String[] args) {
-		ObjectList list = new ObjectList();
+		GList<Integer> list = new GList<>();
 		 list.add(3);
 		 list.add(5);
-		 int size = list.size();
-		 System.out.printf("size : %d\n", size); // clear하기 전 상태
-		 
-		 list.clear();
-		 size = list.size();
-		 System.out.printf("size : %d\n", size); // clear한 후 상태
-		 
+		 list.add(6);
 		 list.add(7);
-		 int num = (Integer) list.get(0);		// Object -> Integer
-		 System.out.printf("num : %d\n", num);	// 0번째 num값 출력
-		 //num = (Integer) list.get(1);			// 1번째 값 읽기 -> 1번째 값 없음  -> IndexOutOfBoundsException 오류 발생
+		 list.add(3);
+		 list.add(5);
+		 list.add(6);
+		 list.add(7);
+		 list.add(3);
+		 list.add(5);
+		 list.add(6);
+		 list.add(7);
+		 
+		 for (int i = 0; i < list.size(); i++) {
+			System.out.printf("%d,", list.get(i));
+		// num = (Integer)list.get(1);
+		}
 	} // end of main
 } // end of Program
 
